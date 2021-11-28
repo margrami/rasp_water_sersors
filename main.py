1bce7cfae58401af91d9be8f88528a103a62cee2
margrami <margrami221@yahoo.com>
2021-11-28 21:20:16 +0100

    incluye matplotlib solo embedded

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, Response
 import numpy as np
 import sys
 import datetime
 import logging
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 from time import sleep
 from MCP3008 import MCP3008
 
@@ -10,7 +13,6 @@ app = Flask(__name__)
 adc = MCP3008()
 channels = [0, 1, 2, 3, 4, 5]
 sleepTime = 1
-
 
 def val(x):
     # function to read the ADC  
@@ -39,6 +41,21 @@ def updatedecimal():
     return jsonify('', render_template('random_decimal_model.html', x = random_decimal, table_data = table_data))
 
 
+
+@app.route ('/plot.png')
+def plot_png():
+	fig = create_figure()
+	output = io.BytesIO()
+	FigureCanvas(fig).print_png(output)
+	return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+	fig = Figure()
+	axis = fig.add_subplot(1, 1, 1)
+	xs = range(100)
+	ys = [random.randint(1, 50) for x in xs]
+	axis.plot(xs, ys)
+	return fig
 
 
 if __name__ == '__main__':
