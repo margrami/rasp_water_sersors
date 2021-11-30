283a9bf00a436c803d07f273d7ff36d35f11ba4e
margrami <margrami221@yahoo.com>
2021-11-30 18:50:08 +0100

    database y grafica funcionando v1

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ db = SQLAlchemy(app)
 
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False)
     time = db.Column(db.DateTime, nullable=False) 
     plantName = db.Column(db.String(32), nullable=False )
     value = db.Column(db.Float, nullable=False)
@@ -29,6 +30,7 @@ class Sensor(db.Model):
     def to_dict(self):
         return {
              'id': self.id,
+             'number': self.number,
              'time': self.time,
              'value': self.value
         }
@@ -56,19 +58,33 @@ def print_dico(dic):
     for k, v in dic.items():
         print(k, v)
 
-
+# lecturas sensores online 
 @app.route('/')
 def index():
-    number = np.random.rand()
+    #number = np.random.rand()
     table_data = map(lambda x: val(x), channels)
-    return render_template('home.html', x = number, table_data = table_data )
+    return render_template('home.html', table_data = table_data )
 
 
 @app.route('/update_decimal', methods=['POST'])
 def updatedecimal():
-    random_decimal = np.random.rand()
+    #randomDecimal = np.random.rand()
+    dateraw = datetime.datetime.now()
+    lectureHour = dateraw.strftime("%y-%m-%d_%H:%M:%S")
     table_data = map(lambda x: val(x), channels)
-    return jsonify('', render_template('random_decimal_model.html', x = random_decimal, table_data = table_data))
+    lecture = Sensor(number=2, 
+                     time=dateraw, 
+                     plantName='albaca', 
+                     value=val(2), 
+                     watering=False, 
+                     wateringML=30)
+    db.session.add(lecture)
+    db.session.commit()
+
+    return jsonify('', render_template('random_decimal_model.html', 
+                       lecture = lectureHour, table_data = table_data))
+
+
 
 
 @app.route('/watering')
@@ -78,18 +94,26 @@ def watering():
 
 @app.route ('/plot.png')
 def plot_png():
-	fig = create_figure()
-	output = io.BytesIO()
-	FigureCanvas(fig).print_png(output)
-	return Response(output.getvalue(), mimetype='image/png')
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 def create_figure():
-	fig = Figure()
-	axis = fig.add_subplot(1, 1, 1)
-	xs = range(100)
-	ys = [np.random.randint(1, 50) for x in xs]
-	axis.plot(xs, ys)
-	return fig
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    #xs = range(100)
+    result = db.engine.execute("SELECT value FROM Sensor WHERE number=2 ORDER BY time")
+    result_as_list = result.fetchall()
+    for row in result_as_list:
+        print(row)
+
+    #ys = [np.random.randint(1, 50) for x in xs]
+    ys = list(result_as_list)
+    xs = range(result_as_list.__len__())
+    print (type(xs), type(ys))
+    axis.plot(xs, ys)
+    return fig
 
 
 if __name__ == '__main__':
