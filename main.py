cddf3137f863231e50db8c30653cbafc31e7f5e2
margrami <margrami221@yahoo.com>
2021-12-03 01:00:38 +0100

    display several plot.png

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from MCP3008 import MCP3008
 app = Flask(__name__)
 
 #database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///media/pi/exdrive1/rasServer/db/wdb.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -37,12 +38,12 @@ class Sensor(db.Model):
     
 
 db.create_all()
-
 #/database
 
 adc = MCP3008()
 channels = [0, 1, 2, 3, 4, 5]
 sleepTime = 1
+
 
 def val(x):
     # function to read the ADC  
@@ -58,33 +59,47 @@ def print_dico(dic):
     for k, v in dic.items():
         print(k, v)
 
-# lecturas sensores online 
+def sensorQuery(sensorNum):
+    return 'SELECT value FROM Sensor WHERE number={0} ORDER BY time'.format(sensorNum)
+
+
+def createFigure(sensorNum):
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    print(sensorQuery(sensorNum))
+    result = db.engine.execute(sensorQuery(sensorNum))
+    result_as_list = result.fetchall()
+    #for row in result_as_list:
+    #    print(row)
+    ys = list(result_as_list) # type is class list
+    xs = range(result_as_list.__len__()) # type is class range
+    axis.grid(True)
+    axis.plot(xs, ys)
+    return fig
+
+
+
 @app.route('/')
 def index():
-    #number = np.random.rand()
     table_data = map(lambda x: val(x), channels)
     return render_template('home.html', table_data = table_data )
 
 
 @app.route('/update_decimal', methods=['POST'])
 def updatedecimal():
-    #randomDecimal = np.random.rand()
     dateraw = datetime.datetime.now()
     lectureHour = dateraw.strftime("%y-%m-%d_%H:%M:%S")
     table_data = map(lambda x: val(x), channels)
-    lecture = Sensor(number=2, 
+    lecture = Sensor(number=3, 
                      time=dateraw, 
                      plantName='albaca', 
-                     value=val(2), 
+                     value=val(3), 
                      watering=False, 
                      wateringML=30)
     db.session.add(lecture)
     db.session.commit()
-
     return jsonify('', render_template('random_decimal_model.html', 
                        lecture = lectureHour, table_data = table_data))
-
-
 
 
 @app.route('/watering')
@@ -92,28 +107,32 @@ def watering():
     return render_template ('home.html', w = 'registrado') 
 
 
-@app.route ('/plot.png')
-def plot_png():
-    fig = create_figure()
+@app.route ('/plot1.png')
+def plot1_png():
+    fig = createFigure(2)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
-def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    #xs = range(100)
-    result = db.engine.execute("SELECT value FROM Sensor WHERE number=2 ORDER BY time")
-    result_as_list = result.fetchall()
-    for row in result_as_list:
-        print(row)
+@app.route ('/plot2.png')
+def plot2_png():
+    fig = createFigure(3)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
-    #ys = [np.random.randint(1, 50) for x in xs]
-    ys = list(result_as_list)
-    xs = range(result_as_list.__len__())
-    print (type(xs), type(ys))
-    axis.plot(xs, ys)
-    return fig
+# def create_figure():
+#     fig = Figure()
+#     axis = fig.add_subplot(1, 1, 1)
+#     result = db.engine.execute(sensorQuery(2))
+#     result_as_list = result.fetchall()
+#     #for row in result_as_list:
+#     #    print(row)
+#     ys = list(result_as_list) # type is class list
+#     xs = range(result_as_list.__len__()) # type is class range
+#     axis.grid(True)
+#     axis.plot(xs, ys)
+#     return fig
 
 
 if __name__ == '__main__':
