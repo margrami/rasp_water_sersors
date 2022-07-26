a682e7cd5519d684d47479a590a25f687e794e2b
margrami <margrami221@yahoo.com>
2022-07-26 20:59:56 +0200

    260722 check some changes

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite://///media/pi/exdrive1/rasServer/db/wdb.sqlite'
-SQLALCHEMY_DATABASE_URI = 'sqlite://///tmp/wdb.sqlite'
+SQLALCHEMY_DATABASE_URI = 'sqlite://///media/pi/EMTU2/rasServer/db/wdb.sqlite'
+
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
